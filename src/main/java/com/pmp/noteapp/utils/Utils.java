12e1d80855bb334ccf65df64ab76b136e3456e60
prashobmp87 <prashob.mp@gmail.com>
2024-02-25 02:47:10 +0400

    Utils

---
+++ src/main/java/com/pmp/noteapp/utils/Utils.java
@@ -0,0 +1,13 @@
+package com.pmp.noteapp.utils;
+
+public class Utils {
+
+	
+	public static boolean isValid(String str) {
+		if(str!=null && !str.isEmpty()){
+			return true;
+
+		}
+		return false;
+	}
+}
